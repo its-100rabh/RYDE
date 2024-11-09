ecb673c336cfccc7115bb2f6ff3bfd9847650a60
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-11-09 22:27:32 +0530

    home route setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import { Redirect } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-    // @ts-ignore
-    return <Redirect href='/(auth)/welcome' />
-}
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+  // @ts-ignore
+  return <Redirect href="/(auth)/welcome" />;
+};
 
 export default Home;
